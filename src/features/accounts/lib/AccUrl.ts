19c09fdae034c1069e110ca2c6761db7a369cf16
Binit <binitkoirala17@gmail.com>
2025-08-11 01:08:25 +0545

    fix: acc url

--- src/features/accounts/lib/AccUrl.ts
+++ src/features/accounts/lib/AccUrl.ts
@@ -1,5 +1,7 @@
 export const getSocialUrl = (
   platform: "facebook" | "tiktok" | "youtube",
   userId: string,
-) =>
-  `https://socially.work/v1/auth/social/${platform}/login/?user_id=${userId}`;
+): string => {
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+    return `${API_BASE_URL}/v1/auth/social/${platform}/login/?user_id=${userId}`;
+};
