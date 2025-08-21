f4c9dfb3ae3cb41429e2c35a9b00a45485954686
Abishkardhenga <copywriterayan47@gmail.com>
2025-08-21 19:56:33 +0545

    fixed the bug

--- src/features/accounts/lib/AccUrl.ts
+++ src/features/accounts/lib/AccUrl.ts
@@ -2,5 +2,5 @@ export const getSocialUrl = (
   platform: "facebook" | "tiktok" | "youtube",
   userId: string,
 ) =>
-  `${import.meta.env.VITE_BACKEND_API_URL}/v1/auth/social/${platform}/login/?user_id=${userId}`;
+  `${import.meta.env.VITE_API_BASE_URL}/v1/auth/social/${platform}/login/?user_id=${userId}`;
   // `https://socially.work/v1/auth/social/${platform}/login/?user_id=${userId}`;
