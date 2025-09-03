0a9dfef64adee8b46f6d4a2fd1448d3bbba45a44
Abishkardhenga <copywriterayan47@gmail.com>
2025-09-03 20:57:32 +0545

    Not shown social media fix

--- src/features/accounts/components/ConnectedAcc.tsx
+++ src/features/accounts/components/ConnectedAcc.tsx
@@ -1,5 +1,17 @@
 import { useAuthUtils } from "@/hooks/useAuthUtils"
-import { Box, Button, Heading, Image, SimpleGrid, Text } from "@chakra-ui/react"
+import {
+  Box,
+  Button,
+  Heading,
+  Image,
+  SimpleGrid,
+  Text,
+  HStack,
+  VStack,
+  Icon,
+} from "@chakra-ui/react"
+import { FaArrowLeft, FaFacebook, FaTiktok, FaYoutube } from "react-icons/fa6"
+
 import FacebookAccount from "../../../components/SocialAcc/facebook/FacebookAccount"
 import TiktokAccount from "../../../components/SocialAcc/tiktok/TiktokAccount"
 import YoutubeAccount from "../../../components/SocialAcc/youtube/YoutubeAccount"
@@ -7,6 +19,9 @@ import { CircularLoading } from "@/lib/loadings"
 import { AccountSection } from "@/components/SocialAcc/AccountSection"
 import { useAllConnAccounts } from "@/hooks/useConnectedAccounts"
 import InstagramAccount from "@/components/SocialAcc/instagram/InstagramAccount"
+import { IoLinkOutline } from "react-icons/io5"
+
+import { getSocialUrl } from "@/features/accounts/lib/AccUrl"
 
 export const ConnectedAcc = () => {
   const { navigate, userId } = useAuthUtils()
@@ -29,39 +44,66 @@ export const ConnectedAcc = () => {
         <CircularLoading />
       </div>
     )
+
+  const facebookUrl = getSocialUrl("facebook", userId)
+  const tiktokUrl = getSocialUrl("tiktok", userId)
+  const youtubeUrl = getSocialUrl("youtube", userId)
   return (
     <>
       {data?.length === 0 ? (
         <Box
-          boxSize={40}
           display="flex"
           alignItems="center"
           justifyContent="center"
           flexDirection="column"
           w="full"
-          mt={20}
+          py={20}
         >
-          <Image
-            src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTMIqiMHpK1y1uDtxqyxQwLbb_fAhM9XQ99rh6SNzYP54Jcr2Uu"
-            alt="placeholder"
-            height="auto"
-            width="18rem"
-            borderRadius="30px"
-          />
-          <Box textAlign="center" mt={5}>
-            <Heading>No connected accounts</Heading>
-            <Text fontSize="sm" color="primary.500">
-              Connect your account to manage your content
+          <Box textAlign="center">
+            <Heading size="xl" mb={4}>
+              Connect your social media accounts
+            </Heading>
+
+            <Box mb={5}>
+              <Icon as={IoLinkOutline} boxSize={12} color="gray.400" />
+            </Box>
+
+            <Text fontSize="lg" color="gray.600" mb={2}>
+              No accounts connected
             </Text>
-            <Button
-              variant="subtle"
-              flex={1}
-              borderRadius="xl"
-              mt={5}
-              onClick={() => navigate("/account/connect")}
-            >
-              Connect account
-            </Button>
+
+            <Text fontSize="sm" color="gray.600" mb={8}>
+              Would you like to connect to the following accounts
+            </Text>
+
+            <HStack gap={4} justifyContent="center">
+              <Button
+                variant="outline"
+                size="lg"
+                onClick={() => (window.location.href = facebookUrl)}
+              >
+                <Icon as={FaFacebook} color="blue.500" />
+                Facebook
+              </Button>
+
+              <Button
+                variant="outline"
+                size="lg"
+                onClick={() => (window.location.href = tiktokUrl)}
+              >
+                <Icon as={FaTiktok} color="black" />
+                Tiktok
+              </Button>
+
+              <Button
+                variant="outline"
+                size="lg"
+                onClick={() => (window.location.href = youtubeUrl)}
+              >
+                <Icon as={FaYoutube} color="red.500" />
+                Youtube
+              </Button>
+            </HStack>
           </Box>
         </Box>
       ) : (
--- src/pages/account/Account.tsx
+++ src/pages/account/Account.tsx
@@ -1,13 +1,23 @@
-import { Box, Button, Flex, Heading } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react"
 
-import { useAuthUtils } from "@/hooks/useAuthUtils";
-import { ConnectedAcc } from "@/features/accounts/components/ConnectedAcc";
-import { Helmet } from "react-helmet-async";
+import { useAuthUtils } from "@/hooks/useAuthUtils"
+import { ConnectedAcc } from "@/features/accounts/components/ConnectedAcc"
+import { Helmet } from "react-helmet-async"
 import { FaPlus } from "react-icons/fa6"
+import { CircularLoading } from "@/lib/loadings"
+
+import { useAllConnAccounts } from "@/hooks/useConnectedAccounts"
 
 export default function Account() {
-  const { navigate } = useAuthUtils()
+  const { navigate, userId } = useAuthUtils()
+  const { data, isLoading } = useAllConnAccounts(userId)
 
+  if (isLoading)
+    return (
+      <div>
+        <CircularLoading />
+      </div>
+    )
   return (
     <>
       <Helmet>
@@ -17,19 +27,24 @@ export default function Account() {
         <Flex justify="space-between" marginTop={6}>
           <Heading size="2xl">{}</Heading>
           {/* <Heading size="2xl">Connected Accounts</Heading> */}
-          <Button
-            variant="subtle"
-            flex={1}
-            backgroundColor={"#003a6b"}
-            borderColor="border.DEFAULT"
-            borderRadius="xl"
-            maxW={40}
-            color={"white"}
-            onClick={() => navigate("/account/connect")}
-          >
-            <FaPlus style={{ height: "14px" }} />
-            Add Account
-          </Button>
+
+          {data.length == 0 ? (
+            <Text></Text>
+          ) : (
+            <Button
+              variant="subtle"
+              flex={1}
+              backgroundColor={"#003a6b"}
+              borderColor="border.DEFAULT"
+              borderRadius="xl"
+              maxW={40}
+              color={"white"}
+              onClick={() => navigate("/account/connect")}
+            >
+              <FaPlus style={{ height: "14px" }} />
+              Add Account
+            </Button>
+          )}
         </Flex>
         <div>
           <ConnectedAcc />
